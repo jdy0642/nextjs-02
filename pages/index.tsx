0e542dd42299972d5eef3289882c6b6dceb40d85
jdy <jdy0642@gmail.com>
2024-06-03 01:40:56 +0900

    이미지 최적화 하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <>home</>
+    <>
+      <Image 
+        src='https://images.unsplash.com/photo-1715553176007-31923bd14f78?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        width={500}
+        height={500}
+        alt=''
+      />
+    </>
   );
 }
