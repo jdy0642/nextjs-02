722e9c79f202da6b7b559b258f3291a741478285
dongyup <dongyup@powertask.biz>
2024-06-04 18:43:29 +0900

    prettier 적용

--- pages/greet/[name].tsx
+++ pages/greet/[name].tsx
@@ -1,15 +1,12 @@
-export const getServerSideProps = async({params}: any) => {
-    const {name} = params;
-    return {
-        props: {
-            name
-        }
-    }
-}
+export const getServerSideProps = async ({ params }: any) => {
+  const { name } = params;
+  return {
+    props: {
+      name,
+    },
+  };
+};
 
 export default function Name(props: any) {
-    return (
-      <>{props.name}</>
-    );
-  }
-  
+  return <>{props.name}</>;
+}
